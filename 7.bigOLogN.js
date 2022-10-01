d8fde82cc0c5767443f3f298ab301c46dc17cca1
Siydulislam <siydulislam333@gmail.com>
2022-10-01 10:30:11 +0600

    Big O notation log n

---
+++ 7.bigOLogN.js
@@ -0,0 +1,15 @@
+// Big O notation is used to determine the performance of an Algorithm
+// Big O notation gives us freedom to determine Time Complexity and Space Complexity
+// We can determine the Time Complexity and Space Complexity with the help of Big O notation
+
+// Comparision between Big O(log n) and O(1)
+
+// Complexity of O(1) is less than O(log n)
+// Takes a little bit more time in O(log n) than O(1)
+
+// Complexity is more in Big O(n!)
+
+// Complexity order =>
+// O(1) < O(log n) < O(n) < O(n log n) < O(n^2) < O(2^n) < O(n!)
+
+// The performance of an algorithm depends on Big O notation, not on hardware!
