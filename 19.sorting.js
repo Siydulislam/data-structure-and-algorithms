56b7ad87c6ba0ca75b68779c753f3bb217fb908f
Siydulislam <siydulislam333@gmail.com>
2022-10-11 13:15:15 +0600

    sorting algorithms

---
+++ 19.sorting.js
@@ -0,0 +1,19 @@
+// Sorting Algorithms
+// Bubble Sort
+// Selection Sort
+// Insertion Sort
+
+// Sorting built-in method
+let arr = [6, 12, 7, 23, -23, 100];
+
+arr.sort();
+arr.sort((a, b) => a - b);
+arr.sort((a, b) => b - a);
+
+console.log(arr);
+
+let nameArr = ['Chattogram', 'Sylhet', 'Feni', 'Bhola', 'Natore'];
+
+nameArr.sort();
+
+console.log(nameArr);
